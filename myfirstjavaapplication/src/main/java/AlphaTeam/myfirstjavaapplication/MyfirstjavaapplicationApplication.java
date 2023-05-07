ea41da48e3b1a4843e770571c00c12891ac28bb6
Ali Shahbazi <alishahbazi799@gmail.com>
2023-05-07 15:05:51 -0400

    Initial commit

---
+++ myfirstjavaapplication/src/main/java/AlphaTeam/myfirstjavaapplication/MyfirstjavaapplicationApplication.java
@@ -0,0 +1,13 @@
+package AlphaTeam.myfirstjavaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstjavaapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstjavaapplicationApplication.class, args);
+	}
+
+}
---
+++ myfirstjavaapplication/src/test/java/AlphaTeam/myfirstjavaapplication/MyfirstjavaapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package AlphaTeam.myfirstjavaapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstjavaapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
