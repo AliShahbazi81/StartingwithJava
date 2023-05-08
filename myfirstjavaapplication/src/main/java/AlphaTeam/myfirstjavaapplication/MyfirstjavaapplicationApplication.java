2d523195b3652201c7501b0a593056849728ae85
VorTex484 <ali.vortex799@gmail.com>
2023-05-08 19:36:33 -0400

    ApplicationPropertiesPractice

--- myfirstjavaapplication/src/main/java/AlphaTeam/myfirstjavaapplication/MyfirstjavaapplicationApplication.java
+++ myfirstjavaapplication/src/main/java/AlphaTeam/myfirstjavaapplication/MyfirstjavaapplicationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyfirstjavaapplicationApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MyfirstjavaapplicationApplication.class, args);
 	}
 
--- myfirstjavaapplication/src/main/java/AlphaTeam/myfirstjavaapplication/rest/FunRestController.java
+++ myfirstjavaapplication/src/main/java/AlphaTeam/myfirstjavaapplication/rest/FunRestController.java
@@ -1,14 +1,34 @@
 package AlphaTeam.myfirstjavaapplication.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    // Injecting values which has been saved in our application.properties
+    @Value("${team.coach}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String teamInfo()
+    {
+        return coachName + " is the coach of " + teamName;
+    }
+
     @GetMapping("/")
     public String sayHello()
     {
         return "Hello World!";
+    }
+
+    @GetMapping ("/Test")
+    public String test()
+    {
+        return "This is a test Controller!";
     }
 }
